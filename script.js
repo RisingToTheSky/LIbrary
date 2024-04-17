66d78a0c10aed7104aafc09ac1679a9989df4d35
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2024-04-17 09:54:29 +0200

    Add dynamic cards using JavaScript

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const myLibrary = [];
+const main = document.getElementsByClassName("main");
+const grid = document.getElementById("grid");
+
+const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
+const theHobbit2 = new Book('The Lord of the Rings', 'J.R.R. Tolkien', '295 pages', 'not read yet');
+const theHobbit3 = new Book('The Silmarillion', 'J.R.R. Tolkien', '295 pages', 'not read yet');
 
 function Book(title, author, pages, hasBeenRead) {
     this.title = title;
@@ -7,9 +13,21 @@ function Book(title, author, pages, hasBeenRead) {
     this.hasBeenRead = hasBeenRead;
 }
 
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet')
-console.log(theHobbit.info());
-
 function addBookToLibrary() {
-
+    myLibrary.push(theHobbit);
+    myLibrary.push(theHobbit2);
+    myLibrary.push(theHobbit3);
 }
+
+function displayLibrary() {
+    for (let i = 0; i < myLibrary.length; i++) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+        grid.appendChild(card);
+    }
+}
+
+addBookToLibrary();
+displayLibrary();
+
+console.log(myLibrary);
