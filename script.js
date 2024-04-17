4b0c249672982edc73941e630195e2ad0acf0486
Joshua <141631716+RisingToTheSky@users.noreply.github.com>
2024-04-17 13:43:50 +0200

    Add blur effect

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const myLibrary = [];
 const main = document.getElementsByClassName("main");
 const grid = document.getElementById("grid");
+const dialog = document.querySelector("dialog");
+const addButton = document.querySelector("button");
+const closeButton = document.getElementById("close");
+const container = document.getElementById("container");
+const form = document.querySelector("form");
 
 const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', '295 pages', 'not read yet');
 const theHobbit2 = new Book('The Lord of the Rings', 'J.R.R. Tolkien', '295 pages', 'not read yet');
@@ -20,12 +25,38 @@ function addBookToLibrary() {
 }
 
 function displayLibrary() {
-    for (let i = 0; i < myLibrary.length; i++) {
+    myLibrary.forEach(Book => {
         const card = document.createElement("div");
         card.classList.add("card");
         grid.appendChild(card);
-    }
+
+        const title = document.createElement("h3");
+        title.textContent = Book.title;
+        card.appendChild(title);
+
+        const author = document.createElement("p");
+        author.textContent = Book.author;
+        card.appendChild(author);
+
+        const pages = document.createElement("p");
+        pages.textContent = Book.pages;
+        card.appendChild(pages);
+        
+        const hasBeenRead = document.createElement("p");
+        hasBeenRead.textContent = Book.hasBeenRead;
+        card.appendChild(hasBeenRead);
+    });
 }
+
+addButton.addEventListener("click", () => {
+    dialog.classList.remove("hidden");
+    dialog.showModal();
+});
+
+
+closeButton.addEventListener("click", () => {
+    dialog.classList.add("hidden");
+});
 
 addBookToLibrary();
 displayLibrary();
